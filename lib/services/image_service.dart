9a6111fbd1716f0179a64b751dc690b3c5bfe505
Carbon2303 <s22030139@itsch.edu.mx>
2026-05-19 23:36:53 -0600

    chatbot2

--- lib/services/image_service.dart
+++ lib/services/image_service.dart
@@ -1,43 +1,79 @@
 class ImageService {
-  static String getImage(String productName) {
-    final name = productName.toLowerCase();
+  static String getImage(String name) {
+    final n = name.toLowerCase();
 
-    if (name.contains('yogurt')) {
-      return 'assets/images/yogurt.png';
+    switch (n) {
+      case "barra_almendra":
+        return "assets/images/barra_almendra.png";
+
+      case "barra_cacahuate":
+        return "assets/images/barra_cacahuate.png";
+
+      case "bebida_soya":
+        return "assets/images/bebida_soya.png";
+
+      case "crema_avellana":
+        return "assets/images/crema_avellana.png";
+
+      case "crema_cacahuate":
+        return "assets/images/crema_cacahuate.png";
+
+      case "croquetas_pescado":
+        return "assets/images/croquetas_pescado.png";
+
+      case "filete_tilapia_sin_gluten":
+        return "assets/images/filete_tilapia_sin_gluten.png";
+
+      case "galletas_cacahuate":
+        return "assets/images/galletas_cacahuate.png";
+
+      case "galletas_libres_huevo":
+        return "assets/images/galletas_libres_huevo.png";
+
+      case "hamburguesa_soya":
+        return "assets/images/hamburguesa_soya.png";
+
+      case "harina-pescado":
+        return "assets/images/harina_pescado.png";
+
+      case "helado_vainilla":
+        return "assets/images/helado_vainilla.png";
+
+      case "leche_almendra":
+        return "assets/images/leche_almendra.png";
+
+      case "mayonesa_vegana_libre_huevo":
+        return "assets/images/mayonesa_vegana_libre_huevo.png";
+
+      case "mezcla_panqueques_sin_gluten":
+        return "assets/images/mezcla_panqueques_sin_gluten.png";
+
+      case "pan_blanco_sin_gluten":
+        return "assets/images/pan_blanco_sin_gluten.png";
+
+      case "pan_integral_libre_huevo":
+        return "assets/images/pan_integral_libre_huevo.png";
+
+      case "pasta_arroz_sin_gluten":
+        return "assets/images/pasta_arroz_sin_gluten.png";
+
+      case "queso_cheddar":
+        return "assets/images/queso_cheddar.png";
+
+      case "salsa_ostion":
+        return "assets/images/salsa_ostion.png";
+
+      case "salsa_soya":
+        return "assets/images/salsa_soya.png";
+
+      case "sopa_instantanea":
+        return "assets/images/sopa_instantanea.png";
+
+      case "yogurt":
+        return "assets/images/yogurt.png";
+
+      default:
+        return "assets/images/default.png";
     }
-
-    if (name.contains('leche')) {
-      return 'assets/images/leche.png';
-    }
-
-    if (name.contains('pan integral')) {
-      return 'assets/images/pan_integral.png';
-    }
-
-    if (name.contains('pan blanco')) {
-      return 'assets/images/pan_blanco.png';
-    }
-
-    if (name.contains('mayonesa')) {
-      return 'assets/images/mayonesa.png';
-    }
-
-    if (name.contains('cacahuate')) {
-      return 'assets/images/cacahuates.png';
-    }
-
-    if (name.contains('carlos v')) {
-      return 'assets/images/carlosv.png';
-    }
-
-    if (name.contains('atun')) {
-      return 'assets/images/atun.png';
-    }
-
-    if (name.contains('salsa soya')) {
-      return 'assets/images/salsasoya.png';
-    }
-
-    return 'assets/images/default.jpg';
   }
 }
